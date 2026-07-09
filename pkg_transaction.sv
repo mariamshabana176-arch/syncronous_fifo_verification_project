8fff193a36a46a0bf2cfedfcc8342b2876e39a41
mariamshabana176-arch <mariamshabana176@gmail.com>
2026-07-09 13:25:55 +0300

    Add FIFO_transaction class with parameters and constraints

---
+++ pkg_transaction.sv
@@ -0,0 +1,23 @@
+package pkg_transaction;
+class FIFO_transaction;
+parameter FIFO_WIDTH = 16;
+parameter FIFO_DEPTH = 8;
+rand logic [FIFO_WIDTH-1:0] data_in;
+rand logic rst_n, wr_en, rd_en;
+logic [FIFO_WIDTH-1:0] data_out;
+ logic wr_ack, overflow;
+ logic full, empty, almostfull, almostempty, underflow;
+int RD_EN_ON_DIST , WR_EN_ON_DIST ;
+
+function new( int RD_EN_ON_DIST = 30 , WR_EN_ON_DIST = 70 );
+this.RD_EN_ON_DIST = RD_EN_ON_DIST;
+this.WR_EN_ON_DIST = WR_EN_ON_DIST;
+endfunction
+
+constraint C {
+    rst_n dist { 1:=95 , 0:=5};
+    wr_en dist { 1:= WR_EN_ON_DIST , 0:= 100 - WR_EN_ON_DIST};
+    rd_en dist { 1:= RD_EN_ON_DIST , 0:= 100 - RD_EN_ON_DIST};
+}
+endclass
+endpackage
